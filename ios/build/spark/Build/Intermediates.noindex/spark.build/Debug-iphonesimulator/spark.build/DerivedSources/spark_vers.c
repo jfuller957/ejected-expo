b434921557b8f35eac4d9b07bfd193af1b998ab8
jfuller957 <jfuller957@gmail.com>
2019-12-10 17:07:06 -0800

    eject2

---
+++ ios/build/spark/Build/Intermediates.noindex/spark.build/Debug-iphonesimulator/spark.build/DerivedSources/spark_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char sparkVersionString[];
+ extern const double sparkVersionNumber;
+
+ const unsigned char sparkVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:spark  PROJECT:spark-1" "\n";
+ const double sparkVersionNumber __attribute__ ((used)) = (double)1.;
---
+++ ios/build/spark/Build/Intermediates.noindex/spark.build/Debug-iphonesimulator/spark.build/Objects-normal/x86_64/spark_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jfuller957/Desktop/Dev_Land/FSA/TeamProjects/eject/spark/ios/build/spark/Build/Intermediates.noindex/spark.build/Debug-iphonesimulator/spark.build/DerivedSources/spark_vers.c
